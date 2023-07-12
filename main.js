8829827be2821203ba3794f50b9c5a8c9976b3c3
Valerie Freeman <valerah7@gmail.com>
2023-07-12 13:14:14 -0500

    session 1

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
-const materials = ['Hylian Rice', 'Big Hearty Truffle', 'Tabantha Wheat', 'Raw Prime Meat', 'Hateno Cheese', 'Bird Egg']
-const meals = []
+const materials = [
+  "Hylian Rice",
+  "Big Hearty Truffle",
+  "Tabantha Wheat",
+  "Raw Prime Meat",
+  "Hateno Cheese",
+  "Bird Egg",
+];
+const meals = [];
 
 const cook = (firstIngredient, secondIngredient) => {
-    if (firstIngredient === 'Hylian Rice' && secondIngredient === 'Big Hearty Radish') {
-        meals.push('Mushroom Rice Balls')
-    }
-}
+  if (
+    firstIngredient === "Hylian Rice" &&
+    secondIngredient === "Big Hearty Truffle"
+  ) {
+    meals.push("Mushroom Rice Balls");
+  }
+};
 
-cook(materials[1], materials[0])
+cook(materials[0], materials[1]);
 
-console.log('MEALS:')
-console.log('---------------')
+console.log("MEALS:");
+console.log("---------------");
 for (const meal of meals) {
-    console.log(meel)
+  console.log(meal);
 }
 
+// = is an ASSIGNMENT operation
+// === is a COMPARISON operation
 
+// Hateno Cheese and a Bird Egg will make a Cheesy Omlette
+// Tabantha Wheat and Hateno Cheese will make a Cheesy Hylian Pizza
+// Raw Prime Meat and Hylian Rice will make Prime Meat and Rice Bowl
